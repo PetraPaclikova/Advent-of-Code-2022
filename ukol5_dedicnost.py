a38d9ff57f45fae22a595326c8b42bb0791bbc9d
Petra <paclikovapetra@gmail.com>
2022-10-30 13:20:06 +0100

    ukol c 5

--- ukol5_dedicnost.py
+++ ukol5_dedicnost.py
@@ -12,16 +12,13 @@ class Nemoc:
         self.pocet_obeti = pocet_obeti
 
 class Koronavirus(Nemoc):
-    def __init__(self, jmeno, inkubacni_doba, pocet_obeti, sireni, varianty):
-        super().__init__(jmeno, inkubacni_doba, pocet_obeti, sireni)
-        self.varianty = [varianty]
-
+    def __init__(self, jmeno, varianta = []):
+        super().__init__(jmeno, inkubacni_doba = 7, pocet_obeti = 0, sireni = "vzducehm")
+        self.varianta = varianta
         
     def pridej_variantu(self, varianta):
-        self.varianta = varianta
-        self.varianty.append(self.varianta)
-        return (self.varianty)
-    
+        self.varianty.append(varianta)
+        
     def __str__(self):
 
         return super().__str__() + f" varianta: {' ,'.join(self.varianty)}"
