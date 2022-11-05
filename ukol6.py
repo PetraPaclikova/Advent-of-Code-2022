b8d5095ca19b2b9eaa3a4ef3cfce8481589a4723
Petra <paclikovapetra@gmail.com>
2022-11-05 20:41:02 +0100

    ukol6

--- ukol6.py
+++ ukol6.py
@@ -1,14 +1,16 @@
 
 with open("znamky.txt", encoding = "utf-8") as znamky:
-    student_znamky = znamky.readlines()
+   student_znamky = znamky.readlines()
+
+student_znamky_bez = [student_znamky.pop(0)]
 
 nova_znamka_list = []
 for znamka in student_znamky:
-    nove_znamky = znamka.replace("1", "A").replace("2", "B").replace("3", "C").replace("4", "D").replace("5","E").replace("Test A", "Test 1").replace("Test B", "Test 2").replace("Test C", "Test 3").replace("Test D", "Test 4").replace("Test E", "Test 5")
-    print(nove_znamky)
+    nove_znamky = znamka.replace("1", "A").replace("2", "B").replace("3", "C").replace("4", "D").replace("5","E")
     nova_znamka_list.append(nove_znamky)
 
 with open("nove_znamky.txt", mode = "w", encoding = "utf-8") as nove_znamky:
+    nove_znamky.writelines(student_znamky_bez)
     nove_znamky.writelines(nova_znamka_list)
 
 
