2bfad0ac658732a8b8251872d0cec81734bc3ac7
Petra <paclikovapetra@gmail.com>
2022-12-04 16:42:30 +0100

    Ao1

--- AoC_01.py
+++ AoC_01.py
@@ -9,4 +9,17 @@ for x in AO_list:
         list.append(soucet)
         soucet = 0
 print(list)
-max(list)
+print(max(list))
+maximum1 = 67658
+index = list.index(int(67658))
+print(index)
+list.pop(215)
+print(max(list))
+maximum2 =67344
+index2 = list.index(int(67344))
+print(index2)
+list.pop(162)
+print(max(list))
+maximum3 = 65156
+top3 = maximum1+maximum2+maximum3
+print(top3)
